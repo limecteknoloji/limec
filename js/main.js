b27ccd5bb329f85d1d204c6cc80491304e887237
Emircan Ok <emircanok@gmail.com>
2016-10-04 00:46:38 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
+retinajs();
+
 $(document).ready(
     function () {
+
+        $('#fullpage').fullpage({
+            anchors      : ['anasayfa', 'projeler', 'markalar', 'oduller', 'haberler', 'biz', 'limec-ile-tanisin'],
+            sectionsColor: ['#FFFFFF', '#FFFFFF', '#CECECE', '#FFFFFF', '#C4C4C4', '#FFFFFF', '#E5E5E5'],
+            css3         : true
+        });
+
         $('#carousel-projects').slick(
             {
-                arrows        : false,
+                arrows        : true,
                 dots          : false,
+                fade          : true,
                 slidesToShow  : 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                prevArrow     : '<button type="button" class="carousel-prev"></button>',
+                nextArrow     : '<button type="button" class="carousel-next"></button>'
             }
         );
+
     }
 );
